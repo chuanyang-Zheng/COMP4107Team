5ee5749d214f5867fb6203d29f41ac50c9d35d44
Pan Feng <1760495180@qq.com>
2020-03-22 18:28:44 +0800

    Pay initi

---
+++ src/PCS/PayMachine/PayMachineHandler.java
@@ -0,0 +1,25 @@
+package PCS.PayMachine;
+
+
+import AppKickstarter.AppKickstarter;
+import AppKickstarter.misc.AppThread;
+import AppKickstarter.misc.MBox;
+import AppKickstarter.misc.Msg;
+import PCS.PCSStarter;
+
+public class PayMachineHandler extends AppThread {
+
+    public PayMachineHandler(String id, PCSStarter pcsStarter){
+        super(id,pcsStarter);
+    }
+
+    public void run() {
+
+    }
+}
+
+
+
+
+
+
